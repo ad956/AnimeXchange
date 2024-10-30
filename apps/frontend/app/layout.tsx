6f39c96add6ae2a41f40292c48bee99a1d507206
Anand Suthar <anandsuthar956@gmail.com>
2024-10-30 22:15:26 +0530

    feat: wrap layout childern with provider

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "./providers";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
-      <body className={``}>{children}</body>
+      <body className={``}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
