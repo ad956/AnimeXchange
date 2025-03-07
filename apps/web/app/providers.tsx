96cd4de92367d8641470dc021c1af96435ca2091
Anand Suthar <anandsuthar956@gmail.com>
2025-03-07 23:21:48 +0530

    add provider with theme and clerk providers

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@components/theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children};</ClerkProvider>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ClerkProvider>{children}</ClerkProvider>
+    </ThemeProvider>
+  );
 }
