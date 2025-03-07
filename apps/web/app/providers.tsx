809d239adc30c7c73c454291b7b39f9921f3cd59
Anand Suthar <anandsuthar956@gmail.com>
2025-03-07 19:27:18 +0530

    feat: add clerk provider

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return { children };
+  return <ClerkProvider>{children};</ClerkProvider>;
 }
